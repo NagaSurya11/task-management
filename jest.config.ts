18059894b2f9d99e841d983e3afd2867410700c9
NagaSurya11 <116377765+NagaSurya11@users.noreply.github.com>
2025-01-31 18:24:24 +0530

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'task-management',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/task-management',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
